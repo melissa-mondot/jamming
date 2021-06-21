23da444f654cd6b082664a336ff0cd77663fac6c
Melissa Mondot <31637708+melissa-mondot@users.noreply.github.com>
2021-06-20 23:03:06 -0700

    Add save playlist functionality

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,30 +11,35 @@ class App extends React.Component {
       searchResults: [
         {
           id: 1,
+          uri: "slak33nr",
           artist_name: "artist1",
           track_name: "track1",
           album_name: "album1",
         },
         {
           id: 2,
+          uri: "spaom2NNJ",
           artist_name: "artist2",
           track_name: "track2",
           album_name: "album2",
         },
         {
           id: 3,
+          uri: "poojnu9oih",
           artist_name: "artist3",
           track_name: "track3",
           album_name: "album3",
         },
         {
           id: 4,
+          uri: "fhtfae5443",
           artist_name: "artist4",
           track_name: "track4",
           album_name: "album4",
         },
         {
           id: 5,
+          uri: "asj5ahyje24",
           artist_name: "artist5",
           track_name: "track5",
           album_name: "album5",
@@ -44,12 +49,14 @@ class App extends React.Component {
       playlistTracks: [
         {
           id: 3,
+          uri: "poojnu9oih",
           artist_name: "artist3",
           track_name: "track3",
           album_name: "album3",
         },
         {
           id: 4,
+          uri: "fhtfae5443",
           artist_name: "artist4",
           track_name: "track4",
           album_name: "album4",
@@ -59,21 +66,24 @@ class App extends React.Component {
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
   }
+
+  // Check if track is in current playlist, if yes, do nothing
+  // else add track to the end of the playlist array
+  // then set state with updated playlist
   addTrack(track) {
-    // Check if track id is in current playlist, if yes, do nothing
-    // else add track to the end of the playlist array
-    // then set state with updated playlist
     if (!this.state.playlistTracks.includes(track)) {
       this.setState((prevState) => ({
         playlistTracks: [...prevState.playlistTracks, track],
       }));
     }
   }
+
+  // Check if track is in current playlist, if no, do nothing
+  // else remove track from the playlist array
+  // then set state with updated playlist
   removeTrack(track) {
-    // Check if track id is in current playlist, if yes, do nothing
-    // else remove track from the playlist array
-    // then set state with updated playlist
     if (this.state.playlistTracks.includes(track)) {
       this.setState((prevState) => ({
         playlistTracks: prevState.playlistTracks.filter(
@@ -82,10 +92,23 @@ class App extends React.Component {
       }));
     }
   }
+
+  // Take in target value from child component
+  // set playlistName state with value
   updatePlaylistName(name) {
     this.setState(() => ({
       playlistName: name,
     }));
+  }
+
+  // map through playlist, all uris to new array
+  savePlaylist() {
+    let newList = [];
+    this.state.playlistTracks.forEach((uri) => {
+      newList.push(uri);
+    });
+    console.log(newList);
+    return newList;
   }
 
   render() {
@@ -105,6 +128,7 @@ class App extends React.Component {
               tracks={this.state.playlistTracks}
               onRemove={this.removeTrack}
               onNameChange={this.updatePlaylistName}
+              onSave={this.savePlaylist}
             />
           </div>
         </div>
@@ -117,5 +141,4 @@ export default App;
 
 // TODO:
 // Not working properly
-// - addTrack does not check for unique id
-// - removeTrack TypeError: this.props.onAdd is not a function
+// Nothing..for the moment
