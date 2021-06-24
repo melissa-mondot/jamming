a6c99415aecc0502be8d3a74f7f56f998ee2f2a8
Melissa Mondot <31637708+melissa-mondot@users.noreply.github.com>
2021-06-23 22:44:00 -0700

    Narrow down error source

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,14 +1,13 @@
 const authApiUrl = `https://accounts.spotify.com/authorize`;
 const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
-let redirectUri = process.env.REACT_APP_REDIRECT_URI;
+const redirectUri = process.env.REACT_APP_REDIRECT_URI;
 const scopes = ["playlist-modify-public", "playlist-modify-private"];
-let userAccessToken;
-
 const authEndpoint = `${authApiUrl}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
   "%20"
 )}&response_type=token&show_dialog=true`;
-
 const searchTrackEndpoint = `https://api.spotify.com/v1/search?type=track&q=`;
+
+let userAccessToken;
 
 const Spotify = {
   getAccessToken() {
@@ -18,7 +17,7 @@ const Spotify = {
     // check for matching token/time in url
     // captures everything after expires_in until encounters &
     if (
-      userAccessToken === undefined &&
+      typeof userAccessToken === undefined &&
       window.location.href.includes("access_token") &&
       window.location.href.includes("expires_in")
     ) {
@@ -29,6 +28,7 @@ const Spotify = {
       console.log(userAccessToken);
       // force window to clear user token at time specified by expires_in(returned in seconds)
       window.setTimeout(() => (userAccessToken = ""), tokenExpiresMatch * 1000);
+      // TODO: I think this issue is here -- because search api returns if input gets one letter. Any more, and GET access is denied. userToken is undefined for both.
       window.history.pushState("User Access Token", null, "/");
     }
     // If no token and no info in the url, then point to auth link
@@ -43,18 +43,18 @@ const Spotify = {
   // TODO: search query doesn't consume data
   // works if hard code token
   search(term) {
-    console.log(authEndpoint);
     const options = {
       method: "GET",
       headers: {
-        // Authorization: `Bearer ${userAccessToken}`,
-        Authorization: `Bearer BQCenlGpreW3FdTMJoui4KOnJ9vPKLQzyYbhnGYwO_zOYZ3aZSngHipYuykbaUPeNZNLyS0tu2LdHln052DCE6oXLSSYXJkHk_JP0Myc4jALtn1VcTUGGAmF-ioJLagdKNFDfypZN0vsM_xKrVrpOLrPSxahN0lelDkuSakUx4rF-HcRbppr_lpAN6XKQNpRKYy88BcnyvKxqxJ9eCfwplvxeI-Y`,
+        Authorization: `Bearer ${userAccessToken}`,
+        // Authorization: `Bearer BQCenlGpreW3FdTMJoui4KOnJ9vPKLQzyYbhnGYwO_zOYZ3aZSngHipYuykbaUPeNZNLyS0tu2LdHln052DCE6oXLSSYXJkHk_JP0Myc4jALtn1VcTUGGAmF-ioJLagdKNFDfypZN0vsM_xKrVrpOLrPSxahN0lelDkuSakUx4rF-HcRbppr_lpAN6XKQNpRKYy88BcnyvKxqxJ9eCfwplvxeI-Y`,
       },
     };
     fetch(searchTrackEndpoint + term, options)
       .then((res) => res.json())
-      .then((res) => console.log(res));
+      .then((res) => console.log(userAccessToken, res));
   },
 };
 
 export default Spotify;
+// console.log(userAccessToken);
