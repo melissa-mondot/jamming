59c75949decd8ecbe8565911415c9dc76e7c1d15
Melissa Mondot <31637708+melissa-mondot@users.noreply.github.com>
2021-06-24 16:53:44 -0700

    Token sets and data returns. TODO: Token in local storage

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -12,7 +12,6 @@ let userAccessToken;
 const Spotify = {
   getAccessToken() {
     if (userAccessToken) {
-      console.log(userAccessToken);
       return userAccessToken;
     }
     // check for matching token/time in url
@@ -25,10 +24,12 @@ const Spotify = {
       let userAccessTokenMatch =
         window.location.href.match(/access_token=([^&]*)/);
       let tokenExpiresMatch = window.location.href.match(/expires_in=([^&]*)/);
-      userAccessToken = userAccessTokenMatch;
-      console.log(userAccessToken);
+      userAccessToken = userAccessTokenMatch[1];
       // force window to clear user token at time specified by expires_in(returned in seconds)
-      window.setTimeout(() => (userAccessToken = ""), tokenExpiresMatch * 1000);
+      window.setTimeout(
+        () => (userAccessToken = ""),
+        tokenExpiresMatch[1] * 1000
+      );
       window.history.pushState(userAccessToken, null, "/");
     }
     // If no token and no info in the url, then point to auth link
@@ -41,20 +42,17 @@ const Spotify = {
     }
   },
   // TODO: search query doesn't consume data
-  // works if hard code token
   search(term) {
     const options = {
       method: "GET",
       headers: {
-        // Authorization: `Bearer ${userAccessToken}`,
-        Authorization: `Bearer BQCenlGpreW3FdTMJoui4KOnJ9vPKLQzyYbhnGYwO_zOYZ3aZSngHipYuykbaUPeNZNLyS0tu2LdHln052DCE6oXLSSYXJkHk_JP0Myc4jALtn1VcTUGGAmF-ioJLagdKNFDfypZN0vsM_xKrVrpOLrPSxahN0lelDkuSakUx4rF-HcRbppr_lpAN6XKQNpRKYy88BcnyvKxqxJ9eCfwplvxeI-Y`,
+        Authorization: `Bearer ${this.getAccessToken()}`,
       },
     };
     fetch(searchTrackEndpoint + term, options)
       .then((res) => res.json())
-      .then((res) => console.log(userAccessToken, res));
+      .then((res) => console.log(res));
   },
 };
 
 export default Spotify;
-// console.log(userAccessToken);
